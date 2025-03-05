ffa577755315a019b64f99f056be67acb617b837
Vishnu Vardhan Udayagiri <udayagirivishnuvardhan@gmail.com>
2025-03-05 14:59:58 +0530

    [05-03-25]

--- src/ArraysPractice.java
+++ src/ArraysPractice.java
@@ -1,56 +1,112 @@
 import java.util.*;
 public class ArraysPractice {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		
-//		Scanner sc = new Scanner(System.in);
-//		System.out.println("Enter no of employees");
-//		int n = sc.nextInt();
-//		sc.nextLine();
-//		String[] arr = new String[n];
-//		System.out.println("Enter employee names");
-//		for(int i=0;i<n;i++)
-//		{
-//			System.out.print((i+1)+":");
-//			arr[i]=sc.nextLine();
-//		}
-//		System.out.println("The Employees are:");
-//		for(String name:arr)
-//		{
-//			System.out.println(name);
-//		}
+	public static void array4d()
+	{
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter no of Organizations");
+		System.out.println("Enter no of Countries");
+		int c=sc.nextInt();
+		System.out.println("Enter no of Organizations in each country");
 		int o = sc.nextInt();
-		System.out.println("Enter no of companies:");
+		System.out.println("Enter no of companies in each organization:");
 		int m = sc.nextInt();
 		System.out.println("Enter no of employees in each company:");
 		int n = sc.nextInt();
 		
-		String arr[][][] = new String[o][m][n];
-		for(int k=0;k<o;k++)
+		String arr[][][][] = new String[c][o][m][n];
+		for(int l=0;l<c;l++)
 		{
-			for(int i=0;i<m;i++)
+			for(int k=0;k<o;k++)
 			{
-				for(int j=0;j<n;j++)
+				for(int i=0;i<m;i++)
 				{
-					System.out.println("Enter employee"+(j+1)+" of company "+(i+1)+" of Organization "+(k+1));
-					arr[k][i][j]=sc.next();
+					for(int j=0;j<n;j++)
+					{
+						System.out.println("Enter employee"+(j+1)+" of company "+(i+1)+" of Organization "+(k+1)+" of Country:"+(l+1));
+						arr[l][k][i][j]=sc.next();
+					}
 				}
 			}
 		}
-		for(String[][] org:arr)
+		for(String [][][] cnt: arr)
 		{
-			System.out.println("Organization:");
-			System.out.println("Companies:");
-			for(String[] comp:org)
-			{	
-				System.out.print(Arrays.toString(comp));
+			for(String[][] org:cnt)
+			{
+				System.out.println("Organization:");
+				System.out.println("Companies:");
+				for(String[] comp:org)
+				{	
+					System.out.print(Arrays.toString(comp));
+				}
+				System.out.println();
 			}
-			System.out.println();
 		}
+	}
+	
+	public static int min(int arr[])
+	{
+		int min=Integer.MAX_VALUE;
+		for(int n:arr)
+		{
+			min=Math.min(n, min);
+		}
+		return min;
+	}
+	
+	
+	public static int maxmin(int arr[])
+	{
+		return 0;
+	}
+	
+	public static void reverse(int arr[])
+	{
+		if(arr.length==0)
+		{
+			System.out.println(-1);
+			return;
+		}
+		int l=0,r=arr.length-1;
+		while(l<=r)
+		{
+			int temp = arr[l];
+			arr[l++]=arr[r];
+			arr[r--]=temp;
+		}
+		System.out.println(Arrays.toString(arr));
+	}
+
+	public static int findDuplicate1ton(int arr[])
+	{
+		for(int i=0;i<arr.length;i++)
+		{
+			int ind = Math.abs(arr[i]);
+			if(arr[ind]<0)
+				return ind;
+			
+			arr[ind]= -arr[ind];
+		}
+		return -1;
+	}
+	
+	public static int sumOfMatrix(int arr[][])
+	{
+		int sum=0;
+		for(int i=0;i<arr.length;i++)
+		{
+			for(int j=0;j<arr[0].length;j++)
+			{
+				sum+=arr[i][j];
+			}
+		}
+		return sum;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//reverse(new int[]{1,2,3,4,5,6});
+		//System.out.println(findDuplicate1ton(new int[]{1,3,1,2,4}));
+		System.out.println(sumOfMatrix(new int[][]{{1,2,3},{4,5,0}}));
 	}
 
 }
