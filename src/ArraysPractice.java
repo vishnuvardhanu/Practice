3928d7389e57fd64ca377a4d5c5a0a8197b599e3
Vishnu Vardhan Udayagiri <udayagirivishnuvardhan@gmail.com>
2025-03-11 16:19:19 +0530

    [11-03-25]

--- src/ArraysPractice.java
+++ src/ArraysPractice.java
@@ -40,6 +40,7 @@ public class ArraysPractice {
 				System.out.println();
 			}
 		}
+		sc.close();
 	}
 	
 	public static int min(int arr[])
@@ -53,9 +54,15 @@ public class ArraysPractice {
 	}
 	
 	
-	public static int maxmin(int arr[])
+	public static void maxmin(int arr[])
 	{
-		return 0;
+		int max = Integer.MIN_VALUE,min = Integer.MAX_VALUE;
+		for(int n:arr)
+		{
+			max=Math.max(max, n);
+			min=Math.min(min, n);
+		}
+		System.out.println("Max:"+max+" min:"+min);
 	}
 	
 	public static void reverse(int arr[])
@@ -87,6 +94,18 @@ public class ArraysPractice {
 		}
 		return -1;
 	}
+	public static int Duplicates(int arr[])
+	{
+		HashMap<Integer,Integer>  hs = new HashMap<>();
+		for(int i=0;i<arr.length;i++)
+		{
+			if(hs.containsKey(arr[i]))
+				return arr[i];
+			
+			hs.put(arr[i],1);
+		}
+		return -1;
+	}
 	
 	public static int sumOfMatrix(int arr[][])
 	{
@@ -101,12 +120,91 @@ public class ArraysPractice {
 		return sum;
 	}
 	
+	public static void jaggedarrays()
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter no of the companies");
+		int m = sc.nextInt();
+		String arr[][] = new String[m][];
+		
+		for(int i=0;i<m;i++)
+		{
+			System.out.println("Enter no of employees in company "+(i+1));
+			arr[i] = new String[sc.nextInt()];
+		}
+		
+		for(int i=0;i<m;i++)
+		{
+			for(int j=0;j<arr[i].length;j++)
+			{
+				System.out.println("Enter name of the employee no: "+(j+1)+" of the company "+(i+1));
+				arr[i][j]=sc.next();
+			}
+		}
+		
+		for(int i=0;i<m;i++)
+		{
+			for(int j=0;j<arr[i].length;j++)
+			{
+				System.out.println("The name of the employee of "+(j+1)+" of company "+(i+1)+": "+arr[i][j]);
+			}
+		}
+		sc.close();
+	}
+	
+	public static void jaggedarrays3d()
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter no of the organizations");
+		int m = sc.nextInt();
+		System.out.println("Enter no of the companies in each organization");
+		int n = sc.nextInt();
+		
+		String arr[][][] = new String[m][n][];
+		for(int i=0;i<m;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				System.out.println("Enter no of employees in company "+(j+1)+" of organization "+(i+1));
+				arr[i][j] = new String[sc.nextInt()];
+			}
+		}
+		
+		
+		for(int i=0;i<m;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				for(int k=0;k<arr[i][j].length;k++)
+				{
+					System.out.println("Enter name of the employee no: "+(k+1)+" of the company "+(j+1)+" of organization "+(i+1));
+					arr[i][j][k]=sc.next();
+				}
+			}
+		}
+		
+		for(int i=0;i<m;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				for(int k=0;k<arr[i][j].length;k++)
+				System.out.println("The name of the employee of "+(k+1)+" of company "+(j+1)+" of organization "+(i+1)+": "+arr[i][j][k]);
+			}
+		}
+		sc.close();
+		
+	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		//reverse(new int[]{1,2,3,4,5,6});
-		//System.out.println(findDuplicate1ton(new int[]{1,3,1,2,4}));
-		System.out.println(sumOfMatrix(new int[][]{{1,2,3},{4,5,0}}));
+		//System.out.println(Duplicates(new int[]{4,3,1,2,4}));
+		//System.out.println(sumOfMatrix(new int[][]{{1,2,3},{4,5,0}}));
+		//maxmin(new int[]{1,2,3,4,5});
+		jaggedarrays3d();
 	}
 
 }
